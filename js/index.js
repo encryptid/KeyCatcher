156be7512d139642ddc56e79d470efb2149df978
Josh Lawson <ordinarycryptid@gmail.com>
2018-06-18 15:39:14 -0400

    rolled back to independent timers and widened code field so that longer keys fit without scrolling

--- js/index.js
+++ js/index.js
@@ -186,24 +186,49 @@ third.addEventListener('click', function() {
     }, 10800000);
 });
 
-// let four = document.getElementById('four');
-// let fourth = document.getElementById('fourth');
-// let cuatro = document.querySelector('.popup.four');
-// fourth.addEventListener('click', function() {
-//   let remove = function () {
-//     cuatro.classList.remove('off');
-//     console.log('reveal!');
-//   }
-//   let text = four.value;
-//   four.select();
-//   document.execCommand('copy');
-//   console.log(`copied ${text}`);
-//   remove();
-//   setTimeout(function() {
-//     cuatro.classList.add('off');
-//   }, 2000);
-//   setTimeout(function() {
-//     console.log('fourth timer started');
-//     bell.play()
-//     }, 10800000);
-// });
+let four = document.getElementById('four');
+let fourth = document.getElementById('fourth');
+let cuatro = document.querySelector('.popup.four');
+fourth.addEventListener('click', function() {
+  let ticker = document.querySelector('#ticker-four p');
+  console.log(ticker);
+
+  let now = new Date().getTime();
+  let countDown = now + 10800000;
+  let counter = 0;
+
+  let timer = setInterval(function () {
+    
+    now = new Date().getTime();
+
+    counter = countDown - now;
+
+    let days = Math.floor(counter / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((counter % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((counter % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((counter % (1000 * 60)) / 1000);
+    console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+    ticker.textContent =`${hours}h ${minutes}m ${seconds}s`;
+    if (counter <= 1) {
+        clearInterval(timer);
+        console.log('stop!');
+    }
+}, 1000);
+
+  let remove = function () {
+    cuatro.classList.remove('off');
+    console.log('reveal!');
+  }
+  let text = four.value;
+  four.select();
+  document.execCommand('copy');
+  console.log(`copied ${text}`);
+  remove();
+  setTimeout(function() {
+    cuatro.classList.add('off');
+  }, 2000);
+  setTimeout(function() {
+    console.log('fourth timer started');
+    bell.play()
+    }, 10800000);
+});
