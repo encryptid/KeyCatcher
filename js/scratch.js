ec6e2a23e990980e391e9bc7e8d6da43e8246e97
Josh Lawson <ordinarycryptid@gmail.com>
2018-06-27 12:28:07 -0400

    looks good. set- and clearInterval work, timer does not stack

--- js/scratch.js
+++ js/scratch.js
@@ -1,6 +1,7 @@
 let time = 0;
 let timer = 0;
 let cd = 0;
+let timeInit;
 let ticker = document.querySelector('.ticker p');
 let start = document.querySelector('.btn-timer');
 let stop = document.querySelector('.btn-stop');
@@ -12,14 +13,14 @@ let convertTime = function(ms) {
     let hours = Math.floor((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((ms % (1000 * 60)) / 1000);
-    console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+    // console.log(`${hours}h ${minutes}m ${seconds}s`);
     ticker.textContent = `${hours}h ${minutes}m ${seconds}s`;
 }
 
 //set time now:
 function setTime() {
     time = new Date().getTime();
-    console.log(`initial time set as: ${time}`)
+    console.log(`initial time set as: ${time}`);
 }
 
 //set time 3 hours from now
@@ -33,7 +34,7 @@ function setTimer() {
 //should it be set to update the value of a variable?
 let timeCheck = function() {
     time = new Date().getTime();
-    console.log(`time: ${time}`);
+    // console.log(`time: ${time}`);
 };
 
 //check time now against timer
@@ -43,20 +44,27 @@ let countdown = function() {
     cd = timer - time;
     console.log(`countdown: ${cd}`);
     convertTime(cd);
-}
+};
+
+let runTimer = function() {
+    console.log('Timer running');
+    timeCheck();
+    countdown();
+};
 
 start.addEventListener('click', function(){
-    console.log('start clicked!')
+    console.log('start clicked!');
+    time = 0;
+    timer = 0;
     setTime();
     setTimer();
-    timeCheck();
-    setInterval(countdown,999);
-})
+    timeInit = setInterval(runTimer, 1000);
+});
 
-stop.addEventListener('click', function(){
-    console.log('stop clicked!');
-    clearInterval(countdown);
-})
+// stop.addEventListener('click', function(){
+//     console.log('stop clicked!');
+//     clearInterval(timeInit);
+// })
 
     // setTime();
     // setTimer();
